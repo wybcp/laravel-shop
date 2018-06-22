8bddd1ca0de2ba712d930ab7c74881eba7442810
Riverside <wangyb65@gmail.com>
2018-06-22 09:13:31 +0800

    管理后台 - 新增、编辑商品

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -15,4 +15,8 @@ Route::group([
     $router->get('users/{id}/edit','UsersController@edit');
 
     $router->get('products', 'ProductsController@index');
+    $router->get('products/create', 'ProductsController@create');
+    $router->post('products', 'ProductsController@store');
+    $router->get('products/{id}/edit', 'ProductsController@edit');
+    $router->put('products/{id}', 'ProductsController@update');
 });
